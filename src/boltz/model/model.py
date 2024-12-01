13d24187bd705d5f4c6dc81286b9cbe10a510dbb
maxall41 <maxall4@protonmail.com>
2024-11-30 22:24:50 -0800

    fixed label

--- src/boltz/model/model.py
+++ src/boltz/model/model.py
@@ -292,7 +292,7 @@ class Boltz1(LightningModule):
             multiplicity_diffusion_train=-1,
             diffusion_samples=-1,
         )
-        label = torch.tensor(batch["label"]).unsqueeze()
+        label = torch.tensor(batch["label"]).unsqueeze(dim=0)
         print(out, label)
         loss = F.binary_cross_entropy(out, label)
         self.log("train/loss", loss)
